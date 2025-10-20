0ac2d101ddd4ff046c552a09707d458b991117c5
VN-PhanQuocBinh <binh26042005@gmail.com>
2025-10-20 10:11:11 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
+const contactRoutes = require('./routes/contact.route');
+
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/contacts', contactRoutes);
 
 app.get('/', (req, res) => {
   res.json({message: "Welcome to contact book application"});
---
+++ app/controllers/contact.controller.js
@@ -0,0 +1,27 @@
+exports.create = (req, res) => {
+    res.json({ message: "create handler" });
+}
+
+exports.findAll = (req, res) => {
+    res.json({ message: "findAll handler" });
+}
+
+exports.findOne = (req, res) => {
+    res.json({ message: "findOne handler" });
+}
+
+exports.update = (req, res) => {
+    res.json({ message: "update handler" });
+}
+
+exports.delete = (req, res) => {
+    res.json({ message: "delete handler" });
+}
+
+exports.deleteAll = (req, res) => {
+    res.json({ message: "deleteAll handler" });
+}
+
+exports.findAllFavorite = (req, res) => {
+    res.json({ message: "findAllFavorite handler" });
+}
